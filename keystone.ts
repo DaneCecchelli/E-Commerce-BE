3502ef5198cddd60227530f7967403b608218412
DaneCecchelli <danececchelli@hotmail.com>
2022-05-28 00:50:35 -0400

    Set up Keystone and began creating Schema

---
+++ keystone.ts
@@ -0,0 +1,56 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: { origin: [process.env.FRONTEND_URL], credentials: true },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL, // TODO: Add data seeding here} }
+    },
+    lists: createSchema({
+      // schema items go here
+      User,
+      Product,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log('SESSION: ', session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQl Query
+      User: 'id name email',
+    }),
+  })
+);
